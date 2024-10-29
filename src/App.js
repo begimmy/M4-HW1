c990382cb1b80570f7dbb8c0d6bd75220d331c3b
begimmy <begaiymzharmatkyzy@gmail.com>
2024-10-29 23:08:05 +0600

    nothing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import Form from "./Components/Form/Form"; 
- 
- 
- 
-function App() { 
-  return ( 
-    <div> 
-       <Form/> 
-    </div> 
-     
-  ); 
-} 
- 
+import { useState } from "react";
+
+function App() {
+  const [num, setNum] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>{num}</h1>
+      <button onClick={() => {
+        if (num < 15) setNum(num + 1);
+      }}>+</button>
+
+      <button onClick={() => {
+        if (num > 0) setNum(num - 1);
+      }}>-</button>
+
+      <button onClick={() => {
+        setNum(0);
+      }}>Res</button>
+
+      <button onClick={() => {
+        if (num + 5 < 15) {
+          setNum(num + 5);
+        } else {
+          setNum(15);
+        }
+      }}>+5</button>
+
+      <button onClick={() => {
+        if (num - 5 < 0) {
+          setNum(0);
+        } else {
+          setNum(num - 5);
+        }
+      }}>-5</button>
+    </div>
+  );
+}
+
 export default App;
